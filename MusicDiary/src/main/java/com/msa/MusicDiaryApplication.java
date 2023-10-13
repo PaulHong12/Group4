514b8b344012bdb4d8b7bfc7ee6b4183e190c7b2
Paul Hong <t50pakpa@gmail.com>
2023-10-13 14:23:29 +0900

    changedprojectname

--- MusicDiary/src/main/java/com/msa/MusicDiaryApplication.java
+++ MusicDiary/src/main/java/com/msa/MusicDiaryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimpleSNSApplication {
+public class MusicDiaryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleSNSApplication.class, args);
+		SpringApplication.run(MusicDiaryApplication.class, args);
 	}
 
 }
