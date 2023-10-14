6ff364873b1dca0506cbb4cf7b4c947b85c0ce40
Paul Hong <t50pakpa@gmail.com>
2023-10-14 15:58:45 +0900

    AddUpdatePost,AddComment,SignupFeature,getFriendList

--- MusicDiary/src/main/java/com/msa/member/controller/MemberController.java
+++ MusicDiary/src/main/java/com/msa/member/controller/MemberController.java
@@ -4,19 +4,20 @@ import com.msa.auth.TokenInfo;
 import com.msa.member.domain.Member;
 import com.msa.member.dto.LoginDto;
 import com.msa.member.dto.SignupDto;
+import com.msa.member.dto.UserDto;
 import com.msa.member.service.MemberService;
+import com.msa.post.dto.PostDto;
 import com.msa.post.dto.ResultDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import java.util.List;
 
 import static org.springframework.http.HttpHeaders.SET_COOKIE;
 
@@ -26,6 +27,13 @@ import static org.springframework.http.HttpHeaders.SET_COOKIE;
 public class MemberController {
 
     private final MemberService memberService;
+
+    @GetMapping("/{userId}")
+    public ResultDto<List<UserDto>> getFriendList(@PathVariable("userId") long userId){
+        
+        return null;
+    }
+
     @PostMapping("/auth/signUp")
     public ResponseEntity<ResultDto<Member>> signUp(@RequestBody SignupDto signupDto) {
         Member newMember = memberService.addUser(signupDto.userName(), signupDto.email(), signupDto.password());
@@ -61,5 +69,6 @@ public class MemberController {
         return ResponseEntity.ok()
                 .body(new ResultDto<>(200, "Logged out successfully", ""));
     }
+
 
 }
